d34fe481351d3db573959c01166be7f992e5278c
Roman <romanmmmelnyk@gmail.com>
2022-04-27 22:55:16 +0300

    Some upgrades for client side of the project

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,48 @@
 import styles from '../styles/Home.module.css'
-import React, {useState} from "react";
+import {useEffect, useState} from "react";
 import Link from "next/link";
 
-export default function Home({ users }) {
-    if(users.success == false) {
+export default function Home() {
+    const [users, setUsers] = useState(null)
+    const [page, setPage] = useState(1);
+    const [isLoading, setLoading] = useState(false)
+    const [isAll, setIsAll] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        fetch(`http://localhost:5000/api/v1/users?page=${page}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setUsers(data.users)
+                setLoading(false)
+            })
+        setPage(page + 1);
+    }, [])
+
+    const fetchObjects = () => {
+        setLoading(true)
+        fetch(`http://localhost:5000/api/v1/users?page=${page}`)
+            .then((res) => res.json())
+            .then((data) => {
+                if(data.page == data.total_pages) {
+                    setIsAll(true)
+                }
+                data.users.map(user => {
+                    users.push(user)
+                })
+                setUsers(users)
+                setLoading(false)
+            })
+            .finally(() => {
+                setUsers(users)
+            })
+        setPage(page + 1);
+    }
+
+    if (isLoading) return <p>Loading...</p>
+    if (!users) return <p>No users data</p>
+
+    if(users.success === false) {
         return (
             <div>
                 <h1>I don't have any users!</h1>
@@ -12,26 +51,34 @@ export default function Home({ users }) {
 
         )
     } else {
-        return (
-            <div>
-                <h1>Here is the list of users</h1>
-                <Link href={'/login'}><a>Create a new user</a></Link>
-                <ul>
-                    {users.users.map(user => (
-                        <li key={'key' + user.id}><Link href={'http://localhost:3000/users/' + user.id}><a>{user.name}</a></Link></li>
-                    ))}
-                </ul>
-            </div>
-        )
+        if(isAll === false) {
+            return (
+                <div>
+                    <h1>Here is the list of users</h1>
+                    <Link href={'/login'}><a>Create a new user</a></Link>
+                    <ul>
+                        {users.map(user => (
+                            <li key={'key' + user.id}><Link href={'http://localhost:3000/users/' + user.id}><a>{user.name}</a></Link></li>
+                        ))}
+                    </ul>
+                    <button onClick={fetchObjects}>Load more</button>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <h1>Here is the list of users</h1>
+                    <Link href={'/login'}><a>Create a new user</a></Link>
+                    <ul>
+                        {users.map(user => (
+                            <li key={'key' + user.id}><Link href={'http://localhost:3000/users/' + user.id}><a>{user.name}</a></Link></li>
+                        ))}
+                    </ul>
+                </div>
+            )
+        }
+
+
     }
 
-}
-
-Home.getInitialProps = async () => {
-    const response = await fetch(`http://localhost:5000/api/v1/users?page=1`)
-    const users = await response.json()
-
-    return {
-        users
-    }
 }
